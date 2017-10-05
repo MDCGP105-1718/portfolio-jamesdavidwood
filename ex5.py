38a84587006366cd3343e8377082bbd9a72c68c2
Admin <admin@ucs.ac.uk>
2017-10-05 10:48:11 +0100

    This is the 5th excerise for Python

---
+++ ex5.py
@@ -0,0 +1,7 @@
+for n in range (99,0,-1):
+    if n == 1:
+        print(f"{n} bottle of beer on the wall, {n} bottle of beer! So take it down, pass it around, no more bottles of beer on the wall!")
+    elif n == 2:
+        print(f"{n} bottle of beer on the wall, {n} bottle of beer! So take it down, pass it around, {n-1} bottle of beer on the wall!")
+    else:
+        print (f"{n} bottles of beer on the wall, {n} bottles of beer. Take one down, pass it around, {n-1} bottles of bear on the wall...")
